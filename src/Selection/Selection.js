299325f0bdf39272e66be6e04cef780e34088bf2
jsl0820 <1154686922@qq.com>
2017-05-05 09:13:33 +0800

    select

---
+++ src/Selection/Selection.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Selection.css';
+
+class Selection extends Component{
+
+	render(){
+
+		return (
+			<a className="selection-box">
+				
+			</a>
+			)
+	}
+}
+
+export default Selection;
