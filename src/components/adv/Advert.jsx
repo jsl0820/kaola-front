7d19e0871f1d4fa33d5f5cf7bfc57818fb98ffaa
jsl0820 <1154686922@qq.com>
2017-05-15 14:45:04 +0800

    advert 倒计时

--- src/components/adv/Advert.jsx
+++ src/components/adv/Advert.jsx
@@ -5,79 +5,104 @@ const ADV_IMG = {
 	src : require('./image/1.webp'), 
 	alt : '限时抢购'
 };
+
 	
 class Adv extends Component{
 
 	constructor(props){
 		super(props);
-
-		this.state = {
-			leftTime:0,
-		};
+		this.state = {h:0, m:0, s:0 };
 	}
 
-	leftTime(time){
-		// let leftTime = (new Date(time))-(new Date);
-		// let hours = parseInt(leftTime/1000/60/60%24,10);
-		// let minutes = parseInt(leftTime/1000/60%60,10);  
-		// let second = parseInt(leftTime/1000%60,10); 
-	}
 
-	showLeftTime(){
+	getLeftTime(){
 		let date = new Date();
-		let curHours = date.getHours();
-		console.log(curHours);
+		let deTime = this.determineHour();
+		let leftTime = deTime-date;
+		let leftSecond = leftTime/1000;
+		let day  = Math.floor(leftSecond/(60*60*24));
+		let hour = Math.floor((leftSecond-day*24*60*60)/3600);
+		let min  = Math.floor((leftSecond-day*24*60*60-hour*3600)/60);  
+		let sec  = Math.floor(leftSecond-day*24*60*60-hour*3600-min*60);
+		
+		return {h:hour, m:min, s:sec};
 	}
 
-	constructTime(){
+	
+
+	constructTime(h){
 
 		let date = new Date();
-		let y = date.get;
+		let y = date.getFullYear();
 		let m = date.getMonth()+1;
-		let d = date.getDate();
-		let h = 
+		let d 
+		if(h == 0){
+			d = date.getDate()+1;
+		}else{
+			d = date.getDate();
+		}
 
+		return  y+'/'+m+'/'+d+'/'+h+':0:0'
 	}
 
 	determineHour(){
 		let date = new Date();
-		console.log(curHours);
+		let curHours = date.getHours();
+		let setingTime 
 
 		if (0<curHours<9){
-			
+			setingTime = this.constructTime(9);
 		}
 
-		if(9<curHours<13){
-			setingTime = 13;
+		if (9<curHours<13){
+			setingTime = this.constructTime(13);
 		}
 
-		if(13<curHours<17){
-			setingTime = 17;
+		if (13<curHours<17){
+			setingTime = this.constructTime(17);
 		}
 
-		if(17<curHours<23){
-
+		if (17<curHours<23){
+		 	setingTime = this.constructTime(0);
 		}
 
+		var setingDate = (new Date(setingTime)).getTime();
+		return setingDate;
+	}
 
-		console.log(setingTime);
+	countDown(){
 
+		var leftTime = this.getLeftTime();
+		var leftHour = leftTime.h;
+		var leftMin  = leftTime.m;
+		var leftSec  = leftTime.s;
+
+		//console.log(leftTime);
+
+		this.setState({
+			h:leftHour,
+			m:leftMin,
+			s:leftSec
+		})	
 	}
 
 
-	componentWillMount(){
-		this.determineHour();	
-		
-			
+	componentDidMount(){
+		setInterval(()=>{
+			this.countDown();
+		}, 1000);
 	}
 
 	render(){
 		let advimg = ADV_IMG;
+		let hours  = this.state.h;
+		let mints  = this.state.m;
+		let secons = this.state.s;
 		return (
 			<a className = "adv" >
 				<img src = {advimg.src} alt =""/>
 				<div className="des">
-					<p>距离本场结束还有01:06:18</p>
+					<p>距离本场结束还有{hours}:{mints}:{secons}</p>
 					<span className="adv-buy">入场疯抢</span>
 				</div>
 			</a>
