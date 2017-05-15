424a8099327b5ba3784e685b5c5738d40acfe9d7
jsl0820 <1154686922@qq.com>
2017-05-15 16:37:37 +0800

    修改判断时间的方法

--- src/components/adv/Advert.jsx
+++ src/components/adv/Advert.jsx
@@ -17,8 +17,8 @@ class Adv extends Component{
 
 	getLeftTime(){
 		let date = new Date();
-		let deTime = this.determineHour();
-		let leftTime = deTime-date;
+		let endTimeStp = this.getEndTimeStp();
+		let leftTime = endTimeStp-date;
 		let leftSecond = leftTime/1000;
 		let day  = Math.floor(leftSecond/(60*60*24));
 		let hour = Math.floor((leftSecond-day*24*60*60)/3600);
@@ -28,9 +28,8 @@ class Adv extends Component{
 		return {h:hour, m:min, s:sec};
 	}
 
-	
 
-	constructTime(h){
+	getEndTime(h){
 
 		let date = new Date();
 		let y = date.getFullYear();
@@ -41,49 +40,32 @@ class Adv extends Component{
 		}else{
 			d = date.getDate();
 		}
-
 		return  y+'/'+m+'/'+d+'/'+h+':0:0'
 	}
 
-	determineHour(){
+	getEndTimeStp(){
+
 		let date = new Date();
-		let curHours = date.getHours();
-		let setingTime 
+		let hours = date.getHours();
+		let endTime
 
-		if (0<curHours<9){
-			setingTime = this.constructTime(9);
+		if(hours<9){
+			endTime = this.getEndTime(9)
+		} else if(hours<13){
+			endTime = this.getEndTime(13)
+		}else if(hours<17){
+			endTime = this.getEndTime(17)
+		}else if(hours<23){
+		 	endTime = this.getEndTime(0)
 		}
-
-		if (9<curHours<13){
-			setingTime = this.constructTime(13);
-		}
-
-		if (13<curHours<17){
-			setingTime = this.constructTime(17);
-		}
-
-		if (17<curHours<23){
-		 	setingTime = this.constructTime(0);
-		}
-
-		var setingDate = (new Date(setingTime)).getTime();
-		return setingDate;
+		
+		var endTimeStp = (new Date(endTime)).getTime();
+		return endTimeStp;
 	}
 
 	countDown(){
-
-		var leftTime = this.getLeftTime();
-		var leftHour = leftTime.h;
-		var leftMin  = leftTime.m;
-		var leftSec  = leftTime.s;
-
-		//console.log(leftTime);
-
-		this.setState({
-			h:leftHour,
-			m:leftMin,
-			s:leftSec
-		})	
+		let leftTime = this.getLeftTime();
+		this.setState(leftTime)	
 	}
 
 
@@ -91,18 +73,16 @@ class Adv extends Component{
 		setInterval(()=>{
 			this.countDown();
 		}, 1000);
+		
 	}
 
 	render(){
 		let advimg = ADV_IMG;
-		let hours  = this.state.h;
-		let mints  = this.state.m;
-		let secons = this.state.s;
 		return (
 			<a className = "adv" >
 				<img src = {advimg.src} alt =""/>
 				<div className="des">
-					<p>距离本场结束还有{hours}:{mints}:{secons}</p>
+					<p>距离本场结束还有{this.state.h}:{this.state.m}:{this.state.s}</p>
 					<span className="adv-buy">入场疯抢</span>
 				</div>
 			</a>
