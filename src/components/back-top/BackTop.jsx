df552ec65a39d692dacb8f530b489812b5e0eaeb
jsl0820 <1154686922@qq.com>
2017-05-16 09:47:15 +0800

    backTop 基础功能

--- src/components/back-top/BackTop.jsx
+++ src/components/back-top/BackTop.jsx
@@ -2,14 +2,41 @@ import React, { Component } from 'react';
 import './style.css';
 
 
-const BackTop = ()=>{
+class BackTop extends Component{
+	
+	constructor(props){
+		super(props);
+		this.state = {
+			hidden:'back-top hidden',
+		};
+	}
 
-	return (
-		<div className="back-top">
-			<i></i>
-			<p>顶部</p>		
-		</div>
-	)
+	componentDidMount(){
+		window.addEventListener('scroll', this.handleScroll.bind(this));
+	}
+
+	handleScroll(){
+		let y = document.body.scrollTop;
+		if(y>500){
+			this.setState({hidden:'back-top'})
+		}else{
+			this.setState({hidden:'back-top hidden'})
+		}
+	}
+
+	goTop(){
+		window.scrollTo(0,0);
+	}
+
+	render(){
+		return (
+			<div className={this.state.hidden} 
+			onClick={this.goTop.bind(this)}>
+				<i></i>
+				<p>顶部</p>		
+			</div>
+		)	
+	}
 }
 
 export default BackTop;
