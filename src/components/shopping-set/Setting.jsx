7958ab938673eccae303e9c8272e1bd2ea547e25
jsl0820 <1154686922@qq.com>
2017-05-19 10:54:20 +0800

    component shop-set

--- src/components/shopping-set/Setting.jsx
+++ src/components/shopping-set/Setting.jsx
@@ -1,9 +1,39 @@
 import React,{ Component } from 'react';
+import Img from '../../static/img/producticon.png';
+import './style.css';
 
 const Setting = ()=>{
+
+	let arrow = {
+		position:'absolute',
+		background:'red',
+		height:'10px',
+		width:'10px',
+		position:'absolute',
+		background:`url(${Img}) no-repeat -64px -89px`,
+		right:'10px',
+		top:'50%',
+		marginTop:'-5px',
+		backgroundSize:'200px'
+	}
+
 	return (
-		<div>
-			22222
+		<div className="shop-set">
+			<div className="shop-set-send">
+				<div>配送 : </div>
+				<span className="shop-set-to"><span className="shop-set-loca">至</span>天津市东河区 满88元包邮</span>
+				<div style={arrow}></div>
+			</div>
+			<div className="shop-set-intro">
+				<div className="shop-set-tit">说明   </div>
+					<ul>
+						<li>商品税费</li>
+						<li>假一赔十</li>
+						<li>7天无忧退货</li>
+						<li>自营报税仓发货</li>
+					</ul>
+					<div style={arrow}></div>
+				</div>
 		</div>
 		)
 }
